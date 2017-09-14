071a884e019adb964a5dfc92ba3ed91330026508
drkrieger <eplyushko@gmail.com>
2017-09-14 09:54:37 -0700

    Game skeleton buildout

--- WordGuessGame/Program.cs
+++ WordGuessGame/Program.cs
@@ -31,6 +31,24 @@ namespace WordGuessGame
         static void Delete()
         {
             //To Do
+        } 
+        //----------------------- Exit Game ---------------------->
+        //This method will let the user exit out of the game.
+        static void ExitGame()
+        {
+            //To Do 
+        }
+        //------------------------ New Game ----------------------->
+        //This method lets the user starts a new game.
+        static void NewGame()
+        {
+            //TO DO
+        }
+        //---------------------- Play Game ------------------------->
+        //This method wil let the user kick off and start playing the game. 
+        static void Play()
+        {
+            //TO DO
         }
     }
 }
