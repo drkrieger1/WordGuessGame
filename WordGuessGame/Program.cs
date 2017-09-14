8cdf3f541f4e0d252afeb1abd6a7cf9967fe1013
drkrieger <eplyushko@gmail.com>
2017-09-14 09:43:54 -0700

    Add project files.

---
+++ WordGuessGame/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace WordGuessGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        //------------------ Game Nav ---------------->
+        //This method runs the game navigation menu.
+        static void GameNav()
+        {
+            //To Do
+        }
+        //--------------------- View Words --------------->
+        //This method displays the words  from the content text file.
+        static void ViewWord()
+        {
+            //To Do
+        }
+        //----------------------- Add Words ------------------>
+        // This method adds words to the content text file.
+        static void AddWords()
+        {
+            //To Do
+        }
+        //------------------------ Delete ----------------------->
+        // This method Removes words frome the content text file.
+        static void Delete()
+        {
+            //To Do
+        }
+    }
+}
